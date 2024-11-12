f385e36bae5d51a2e32be276031a81de12ada991
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-11-12 21:29:43 +0500

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="bg-black text-white h-screen flex items-center justify-center">
+        <h1 className="text-6xl">React E-commerce Project</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;

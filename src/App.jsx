2b9c7a3eb08f693cc8578d1b7ffa77aeb1acb9f6
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-11-21 22:19:16 +0500

    add configartaion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
+import Banner from "./Components/Banner";
 function App() {
   return (
     <>
-      <div className="bg-black text-white h-screen flex items-center justify-center">
-        <h1 className="text-6xl">React E-commerce Project</h1>
-      </div>
+     <Banner/>
     </>
   );
 }

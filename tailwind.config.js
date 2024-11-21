7101a633e49c15cffa20a40dde46ba1b4c79d009
Sufyan Ahmed Dev <sufiyan033130@gmail.com>
2024-11-21 22:19:34 +0500

    banner adding

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      width: {
+        '7/10': '70%',
+        '3/10': '30%',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        marquee: 'marquee 20s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
